15c00f49400f12d0cbabbec6d8c59d7b53e6c960
haruhiko28 <not.hisano.soccer@gmail.com>
2025-04-28 23:53:46 +0900

    20250428

---
+++ ch03/samples/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+fn m() {
+    let a = 10;
+    let b = 20;
+    {
+        let c = 30;
+        let d = 40;
+    }
+}
+
+fn n() {
+    let e = 50;
+    let f = 60;
+}
