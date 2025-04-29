511cd81906d7bbcd8e189cdc4bf0c1f91c2bc41e
haruhiko28 <not.hisano.soccer@gmail.com>
2025-04-29 23:37:06 +0900

    20250429

--- ch03/samples/src/main.rs
+++ ch03/samples/src/main.rs
@@ -15,3 +15,21 @@ fn n() {
     let e = 50;
     let f = 60;
 }
+
+struct H2O {}
+struct O2 ()
+struct H2 {}
+
+fn burn(_h2_1: H2, _h2_2: H2, _o2: O2) -> (H2O, H2O) {
+    (H2O {}, H2O {})
+}
+
+let h2_1 = H2 {};
+let h2_2 = H2 {};
+let o2 = O2 {};
+
+let (h2o_1, h2o_2) = burn(h2_1, h2_2, o2);
+
+// 既に消費した分子は使えない
+// let (h2o_1, h2o_2) = burn(h2_1, h2_2, o2);
+ 
