63b0778c321e3a9676df270a2c26023ef8b44807
haruhiko28 <not.hisano.soccer@gmail.com>
2025-05-10 00:37:00 +0900

    20250409

--- ch03/samples/src/main.rs
+++ ch03/samples/src/main.rs
@@ -1,7 +1,3 @@
-fn main() {
-    println!("Hello, world!");
-}
-
 fn m() {
     let a = 10;
     let b = 20;
@@ -17,33 +13,41 @@ fn n() {
 }
 
 struct H2O {}
-struct O2 ()
+struct O2 {}
 struct H2 {}
 
 fn burn(_h2_1: H2, _h2_2: H2, _o2: O2) -> (H2O, H2O) {
     (H2O {}, H2O {})
 }
+fn main() {
+    let h2_1 = H2 {};
+    let h2_2 = H2 {};
+    let o2 = O2 {};
 
-let h2_1 = H2 {};
-let h2_2 = H2 {};
-let o2 = O2 {};
+    let (h2o_1, h2o_2) = burn(h2_1, h2_2, o2);
 
-let (h2o_1, h2o_2) = burn(h2_1, h2_2, o2);
+    // 既に消費した分子は使えない
+    // let (h2o_1, h2o_2) = burn(h2_1, h2_2, o2);
+    
+    struct Coin {}
 
-// 既に消費した分子は使えない
-// let (h2o_1, h2o_2) = burn(h2_1, h2_2, o2);
- 
- struct Coin {}
+    let a = Coin {};
+    let b = a;
+    let c = b;
 
- let a = Coin {};
- let b = a;
- let c = b;
+    // let d = a;
 
- // let d = a;
+    let a;
+    {
+        let b = 10;
+        a = &b;
+    }
+    println!("{}", a);
 
- let a;
- {
-    let b = 10;
-    a = &b;
- }
- println!("{}", a);
+    let a;
+    {
+        let b = 10;
+        a = &b;
+        println!("{}", a);
+    }
+}
